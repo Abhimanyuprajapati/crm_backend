d595d1f46752519d6b73025f3d96cb7ce8c37359
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2026-02-19 19:09:19 +0530

    added the cros for login website

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import authRoutes from './routes/auth.route.js';
-import { connectDB } from './lib/db.js';
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
-
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 1111;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://crm-backend-topaz-xi.vercel.app/",
+      "https://crm-backend-topaz-xi.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World to abhi!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World to abhi!");
+});
 
 // auth routes
-app.use('/v1/auth', authRoutes)
-
-// app.post('/post', (req, res) => {
-//   console.log(req.body);
-//   res.send('successfully posted data')
-// })
-
+app.use("/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`)
+  console.log(`http://localhost:${PORT}`);
   connectDB();
-})
+});
